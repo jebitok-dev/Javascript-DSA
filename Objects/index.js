ed5eb4770d6f5c566edd3f8954f166be9324d71a
jebitok-dev <sharonkosgei4@gmail.com>
2023-01-30 23:06:08 +0300

    Add Key-Value Pairs in Objects

--- Objects/index.js
+++ Objects/index.js
@@ -1,10 +1,15 @@
 const person = {
-  firstName: 'Tom', age: 28, hobbies: ['swimming', 'music', 'coding']
+  firstName: 'Tom', age: 28, hobbies: ['swimming', 'music', 'coding'], greet() {console.log('Hi, I am ' + this.firstName + " " + this.lastName)}
 }
+
+person.lastName = 'Aquils'
 
 // can't iterate
 console.log(person.age)
 console.log(person.firstName)
 console.log(person.hobbies[1])
+console.log(person)
+// method
+person.greet()
 
 
