324bb213e1314745cae7231fb18d2cbd190e944e
jebitok-dev <sharonkosgei4@gmail.com>
2023-01-30 22:50:04 +0300

    Add Key-Value Pairs in Objects

---
+++ Objects/index.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: 'Tom', age: 28, hobbies: ['swimming', 'music', 'coding']
+}
+
+// can't iterate
+console.log(person.age)
+console.log(person.firstName)
+console.log(person.hobbies[1])
+
+
