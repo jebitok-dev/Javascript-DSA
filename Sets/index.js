067c1c8459d3aaed27940ee9a049792f858dc7eb
jebitok-dev <sharonkosgei4@gmail.com>
2023-01-30 22:29:58 +0300

    Add Sets Iteration

---
+++ Sets/index.js
@@ -0,0 +1,15 @@
+// new Set();
+// set.add('Pizza')
+// set.add('burger')
+// set.add('pizza') 
+
+const ids = new Set();
+
+ids.add('abc');
+ids.add(1);
+ids.add('bb2');
+ids.add(1);
+
+for(const el of ids) {
+  console.log(el)
+}
