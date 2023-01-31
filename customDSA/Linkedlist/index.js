369b53eed113528febbdfefb6250f507dd06dc9b
jebitok-dev <sharonkosgei4@gmail.com>
2023-01-31 23:18:04 +0300

    Add Deleting & Prepending of LinkedList

--- customDSA/Linkedlist/index.js
+++ customDSA/Linkedlist/index.js
@@ -1,6 +1,80 @@
+// nodes (head node- prepend)5 -> 'hi' -> 8 -> 11 (tail node- append)
+
 class LinkedList {
-  constructor() {
-    this.nodes = [];
+  constructor(value) {
+    this.head = value; // first element of the list 
+    this.next = null; // last element of the list
+  }
+
+  append(value) {
+    const newNode = {value: value, next: null}
+    if(this.tail) {
+      this.tail.next = newNode;
+    } 
+    this.tail = newNode;
+    if(!this.head) {
+      this.head = newNode
+    }
+  }
+
+  prepend(value) {
+    const newNode = {value: value, next: this.head};
+
+    this.head = newNode;
+    if(!this.tail) {
+      this.tail = newNode;
+    }
+  }
+
+  delete(value) {
+    if(!this.head) {
+      return;
+      // throw err
+    }
+
+    while(this.head && this.head.value === value) {
+      this.head = this.head.next;
+    }
+
+    let curNode = this.head;
+
+    while(curNode.next) {
+      if(curNode.next.value === value) {
+        curNode.next = curNode.next.next
+      } else {
+        curNode = curNode.next;
+      }
+    }
+
+    if(this.tail.value === value) {
+      this.tail = curNode;
+    }
+
+  }
+
+  toArray() {
+    const el = [];
+
+    let curNode = this.head;
+    while (curNode) {
+      el.push(curNode);
+      curNode = curNode.next;
+    } 
+
+    return el;
   }
 }
-// nodes (head node- prepend)5 -> 'hi' -> 8 -> 11 (tail node- append)
+
+const linkedList1 = new LinkedList();
+
+linkedList1.append(1);
+linkedList1.append('Hello World!');
+linkedList1.append('Sharon');
+linkedList1.append(false);
+linkedList1.append(18.55);
+linkedList1.prepend('WAGMI')
+
+console.log(linkedList1.toArray())
+
+linkedList1.delete('Sharon')
+linkedList1.delete('WAGMI')
