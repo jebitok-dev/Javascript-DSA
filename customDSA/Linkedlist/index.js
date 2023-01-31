8bf841d03e8256afba35ee525c020aa0622d49bf
jebitok-dev <sharonkosgei4@gmail.com>
2023-01-31 22:22:39 +0300

    Add LinkedList

---
+++ customDSA/Linkedlist/index.js
@@ -0,0 +1,6 @@
+class LinkedList {
+  constructor() {
+    this.nodes = [];
+  }
+}
+// nodes (head node- prepend)5 -> 'hi' -> 8 -> 11 (tail node- append)
--- in-built-DSA/Sets/index.js
+++ in-built-DSA/Sets/index.js
@@ -4,14 +4,19 @@
 // set.add('pizza') 
 
 const ids = new Set();
-
+// add values to set
 ids.add('abc');
 ids.add(1);
 ids.add('bb2');
 ids.add(1);
-
+// iterate through sets 
 for(const el of ids) {
   console.log(el)
 }
 
+// has property in sets
+
 console.log(ids.has(1))
+// delete values from sets
+ids.delete('abc')
+console.log(ids.has('abc'))
