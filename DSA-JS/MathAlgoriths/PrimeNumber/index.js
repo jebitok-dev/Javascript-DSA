829768599d2646c8e37b3f4cae3ea38f88543c00
jebitok-dev <sharonkosgei4@gmail.com>
2023-02-04 22:28:52 +0300

    Add PrimeNumbers

---
+++ DSA-JS/MathAlgoriths/PrimeNumber/index.js
@@ -0,0 +1,23 @@
+// given a natural number n determine if the number is prime or not
+
+function isPrime(n) {
+  if(n < 2) {
+    return false
+  } 
+  for(let i = 2; i < n; i++) {
+    if(n % i === 0) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+console.log(isPrime(3)) // true
+console.log(isPrime(7)) // true
+console.log(isPrime(15)) // true
+console.log(isPrime(18)) // false
+console.log(isPrime(23)) // true
+console.log(isPrime(27)) // true
+console.log(isPrime(100)) // false
+console.log(isPrime(167)) // true
+
