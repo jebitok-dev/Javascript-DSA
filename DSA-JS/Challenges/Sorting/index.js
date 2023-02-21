0fb9bd8184abffdfcc9ff4863088143200c4e754
jebitok-dev <sharonkosgei4@gmail.com>
2023-02-21 22:54:29 +0300

    Add BubbleSort

---
+++ DSA-JS/Challenges/Sorting/index.js
@@ -0,0 +1,22 @@
+// Given an array of integers sort the array
+
+function bubbleSort(arr) {
+  let swapped
+
+  do {
+    swapped = false
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] > arr[i+1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+  while(swapped)
+}
+
+const arr = [-6, 20, 8, -2, 4]
+bubbleSort(arr)
+console.log(arr) // [-6, -2, 4, 8, 20]
