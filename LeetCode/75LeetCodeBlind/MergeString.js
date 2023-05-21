17f98237ec02ba26b4e4df40f6c205a455736c52
jebitok-dev <sharonkosgei4@gmail.com>
2023-05-21 16:39:19 +0300

    Add 75 LeetCodeBlind

---
+++ LeetCode/75LeetCodeBlind/MergeString.js
@@ -0,0 +1,21 @@
+function mergeAltString(word1, word2) {
+  let result = "";
+
+  const maxLength = Math.max(word1.length + word2.length);
+
+  for(let i = 0; i < maxLength.length; i++) {
+    if(i < word1.length) {
+      result += word1[i];
+    }
+
+    if(i < word2.length) {
+      result += word2[i];
+    }
+  }
+  return result;
+  console.log("first", result)
+}
+console.log("first")
+
+
+console.log(mergeAltString("abc", "qrstv"));
